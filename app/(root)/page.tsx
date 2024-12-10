732a84ca5b7045a731dfb698af6433691d78e3fc
YeYint009 <blackskym96@gmail.com>
2024-12-10 17:37:46 +0900

    Setup nextjs15

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup <br/> Connect with Enterpreneurs</h1>
+
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches , and Get Noticed in Virtual Competations</p>
+      </section>
+    </>
+  );
+};
+
+export default Home;
